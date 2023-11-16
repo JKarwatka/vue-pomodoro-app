58658c4f16d76abfae4062576c85b3b1f9b5648b
Jakub Karwatka <jakubkarwatka@gmail.com>
2023-11-16 16:54:40 +0100

    add BaseSwitch and BaseSwitchOption components

---
+++ src/components/BaseSwitch.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import BaseSwitchOption from './BaseSwitchOption.vue';
+import {useSwitchStore} from '../stores/SwitchStore'
+
+const store = useSwitchStore()
+
+</script>
+
+<template>
+  <div class="container">
+    <BaseSwitchOption v-for="option in store.switchOptions" v-bind:key="option.id" :label="option.label" :id="option.id" :isActive="store.activeSwitchOptionId === option.id"/>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  display: flex;
+  flex-direction: row;
+  background-color: #161932;
+  padding: 10px;
+  border-radius: 31.5px;
+  column-gap: 10px;
+}
+</style>
---
+++ src/components/BaseSwitchOption.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import BaseBody1 from './BaseBody1.vue';
+interface BaseSwitchOptionProps {
+  id: string,
+  label: string,
+  isActive: boolean
+}
+
+const { label, isActive } = defineProps<BaseSwitchOptionProps>()
+</script>
+
+<template>
+  <button :class="{active: isActive, inactive: !isActive }">
+    <BaseBody1 :text="label" />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+button{
+  padding: 15px 25px;
+  border-radius: 26px;
+  border: none;
+
+  &:hover {
+    opacity: 0.20;
+  }
+}
+
+.active{
+  color: #1E213F;
+  background-color: #F87070;
+}
+.inactive{
+  color: #D7E0FF;
+  background-color: transparent;
+}
+</style>
